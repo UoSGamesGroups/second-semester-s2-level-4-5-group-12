81e6da6c2c6e8dd1f4b6b208b523bf94442fea28
ElliotChester <elliotchester97@gmail.com>
2017-02-09 23:00:05 +0000

    1st prototype

---
+++ Assets/Code/prototype 1/Assets/spawner.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour {
+
+    [Header("Turret Variables")]
+    public float rotateSpeed;
+
+    [Header ("Water Variables")]
+    public GameObject waterCircle;
+    public float waterPower;
+    public Color waterColour;
+
+    [Header("Key Binds")]
+    public KeyCode upKey;
+    public KeyCode downKey;
+    public KeyCode shootKey;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+
+    private void Update()
+    {
+        Move();
+        Shoot();
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(upKey))
+        {
+            this.transform.Rotate(new Vector3(0,0, rotateSpeed));
+        }
+        if (Input.GetKey(downKey))
+        {
+            this.transform.Rotate(new Vector3(0, 0, -rotateSpeed));
+        }
+    }
+
+    void Shoot()
+    {
+        if (Input.GetKey(shootKey))
+        {
+            StartCoroutine(spawnWater());
+        }
+    }
+
+    IEnumerator spawnWater()
+    {
+        yield return new WaitForSeconds(0.001f);
+        Vector3 spawnAngle = new Vector3(0, 0, Random.Range(0, 2) - 1f) + transform.rotation.eulerAngles;
+        GameObject newWaterCircle = Instantiate(waterCircle, transform.position, Quaternion.Euler(spawnAngle));
+        newWaterCircle.GetComponent<waterscript>().SetSpeed(waterPower);
+        newWaterCircle.GetComponent<waterscript>().SetColour(waterColour);
+
+    }
+}
---
+++ Assets/Code/prototype 1/Assets/waterscript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waterscript : MonoBehaviour {
+    Rigidbody2D RB;
+    Vector3 moveDir;
+    CircleCollider2D Coll;
+    public bool expanding;
+    public float maximumRadius;
+    public float expandSpeed;
+    // Use this for initialization
+    void Start () {
+        RB = this.gameObject.GetComponent<Rigidbody2D>();
+        RB.velocity = transform.rotation * (moveDir);
+        Coll = this.GetComponent<CircleCollider2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Expand();
+	}
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    public void SetSpeed(float speed)
+    {
+        moveDir = new Vector3(speed, 0, 0);
+    }
+    public void SetColour(Color color)
+    {
+        this.GetComponent<SpriteRenderer>().color = color;
+    }
+
+    void Expand()
+    {
+        if (!expanding) { return; }
+        if (Coll.radius < maximumRadius)
+        {
+            Coll.radius = Coll.radius + Time.deltaTime * expandSpeed;
+        }
+        
+    }
+}
